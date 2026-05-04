d70a167ae1858da6539b2d7c7dafa40978d32dc4
Hofmann250 <johan.hofmann250@gmail.com>
2026-05-04 19:08:13 +1000

    add Plant (plug)

---
+++ Plant.java
@@ -0,0 +1,17 @@
+import greenfoot.*;
+
+
+public class Plant extends AnimatedActor {
+    private int health = 300; 
+
+    public Plant(ReanimManager manager, String key) {
+
+        super(manager, key, "anim_idle");
+    }
+
+    public void act() {
+       
+        super.act();
+    }
+
+}
