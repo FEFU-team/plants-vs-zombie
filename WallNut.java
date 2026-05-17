7c9b716be14575ea117a5a009c69cb7ef552d317
Vladimir E. Tkachuk <vltkachuk07@gmail.com>
2026-05-17 11:40:33 +1000

    feat: add changing wall-nut texture according to health

--- WallNut.java
+++ WallNut.java
@@ -16,4 +16,15 @@ public class WallNut extends Plant {
         // TODO: update body image on damage
         // TODO: maybe stop animation while it's attacked like in original game
     }
+    
+    @Override
+    public void takeDamage(float amount) {
+        super.takeDamage(amount);
+        
+        if (health < maxHealth * 0.3) {
+            addImageSwap("IMAGE_REANIM_WALLNUT_BODY", "IMAGE_REANIM_WALLNUT_CRACKED2");
+        } else if (health < maxHealth * 0.6) {
+            addImageSwap("IMAGE_REANIM_WALLNUT_BODY", "IMAGE_REANIM_WALLNUT_CRACKED1");
+        }
+    }
 }
